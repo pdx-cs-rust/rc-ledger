c56815ef5b4d105afcaffdec6146d6c90e60cd5b
Bart Massey <bart.massey@gmail.com>
2022-01-17 13:42:46 -0800

    updated to show refcount management

--- rc.rs
+++ rc.rs
@@ -1,35 +1,78 @@
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::io::{Write, stdout};
+use std::rc::Rc;
+
+#[derive(Default)]
+struct Count(i64);
+
+impl Drop for Count {
+    fn drop(&mut self) {
+        let _ = writeln!(stdout(), "counter dropped");
+    }
+}
+
+impl Count {
+    fn adjust(&mut self, adj: i64) {
+        self.0 += adj;
+    }
+
+    fn value(&self) -> i64 {
+        self.0
+    }
+}
+
+#[derive(Default)]
+struct Counter(Rc<RefCell<Count>>);
+
+impl Counter {
+    fn clone(&self) -> Self {
+        Self(Rc::clone(&self.0))
+    }
+
+    fn adjust(&self, adj: i64) {
+        self.0.borrow_mut().adjust(adj);
+    }
+
+    fn value(&self) -> i64 {
+        self.0.borrow_mut().value()
+    }
+}
 
 struct Assets {
     count: u64,
-    total: Rc<RefCell<i64>>,
+    total: Counter,
 }
 
 struct Debits {
     count: u64,
-    total: Rc<RefCell<i64>>,
+    total: Counter,
 }
 
 impl Assets {
     fn acquire(&mut self) {
-        *self.total.borrow_mut() += 1;
+        self.total.adjust(1);
         self.count += 1;
     }
 }
 
 impl Debits {
     fn loan(&mut self) {
-        *self.total.borrow_mut() -= 1;
+        self.total.adjust(-1);
         self.count += 1;
     }
 }
 
 fn main() {
-    let counter = Rc::new(RefCell::new(0));
-    let mut debits = Debits { count: 0, total: Rc::clone(&counter) };
-    let mut assets = Assets { count: 0, total: Rc::clone(&counter) };
+    let counter = Counter::default();
+    let mut debits = Debits { count: 0, total: counter.clone() };
+    let mut assets = Assets { count: 0, total: counter.clone() };
     assets.acquire();
     debits.loan();
-    println!("{}", *counter.borrow());
+    println!("{}", counter.value());
+    drop(assets);
+    println!("assets dropped");
+    drop(debits);
+    println!("debits dropped");
+    drop(counter);
+    println!("finished");
 }
